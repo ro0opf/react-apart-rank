aff9228f92f2cf62320498d732aad8a261fcde91
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-07-22 10:58:45 +0900

    update body

--- client/src/ui/Body.js
+++ client/src/ui/Body.js
@@ -12,25 +12,41 @@ function Body() {
             {
                 "address1": "충청남도", "address2": "당진시",
                 "address3": "신평면", "address4": "거산리",
-                "apartName": "풍림아이원", "apartEquilibrium": "84.9768"
+                "apartName": "풍림아이원", "area": "84.9768",
+                "price" : "2530000000"
             },
-            { "address1": "광주광역시", "address2": "북구", "address3": "운암동", "apartName": "우미1", "apartEquilibrium": "59.97" }
+            { "address1": "광주광역시", "address2": "북구", "address3": "운암동", "apartName": "우미1", "area": "59.97", "price" : "5500000000" }
         ]
+        
+        function parseEquillibrium(area){
+            const equillibrium = parseFloat(area) / 3.3; 
+            return equillibrium.toFixed(2) + "평";
+        }
+
+        function parsePrice(price){
+            return parseInt(price) / 100000000 + "억";
+        }
 
         return (
             <ul className="ApartRank">
-                <hr/>
+                <hr />
                 {apartList.map((apart, index) =>
                     <>
                         <li onClick={() => apartClicked(apart)}>
-                            {index + 1} {apart.apartName}
-                            <small>
-                                <span>
+                            <div className="ApartRank">
+                                {index + 1}
+                            </div>
+                            <div className="ApartAddress">
+                                {apart.apartName} ({parseEquillibrium(apart.area)})<br/>
+                                <small>
                                     {apart.address1}
-                                </span>
-                            </small>
+                                </small>
+                            </div>
+                            <div className="ApartInfo"> 
+                                {parsePrice(apart.price)}
+                            </div>
                         </li>
-                        <hr/>
+                        <hr />
                     </>
                 )}
             </ul>
