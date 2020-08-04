2e0a231d99492cd1ee086df5c7c337cb7e985984
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-08-04 18:10:32 +0900

    Update Aparts.js

--- client/src/data/Aparts.js
+++ client/src/data/Aparts.js
@@ -1,16 +1,32 @@
-import React, { } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useAsync } from '../util/useAsync';
 import { Link } from 'react-router-dom';
 import { Wrapper } from './Aparts.css'
 
+
+let count = 1;
 async function getAsyncAparts(keyword) {
     const response = await axios.get('http://116.123.85.116:9999/apart', {
         params: {
             apart_name: keyword
         },
     });
+    
+    return response.data;
+}
 
+function test(keyword){
+    const response = axios.get('http://116.123.85.116:9999/apart', {
+        params: {
+            apart_name: keyword
+        },
+    });
+    
+    console.log("LOG >> " + keyword);
+    count++;
+
+    console.log(response);
     return response.data;
 }
 
@@ -20,45 +36,62 @@ function suggestionSelected(value) {
 
 function Aparts(props) {
     const keyword = props.keyword;
-    const [state, refetch] = useAsync(() => getAsyncAparts(props.keyword), [props.keyword]);
-    const { loading, data: aparts, error } = state;
+    // const [state, refetch] = useAsync(() => getAsyncAparts(props.keyword), [props.keyword]);
+    // const { loading, data: aparts, error } = state;
 
-    if (keyword.length < 2) {
-        return null;
-    }
+    // test(keyword);
+    console.log("keyword is " + keyword);
+    useEffect(()=>{
+        const response = axios.get('http://116.123.85.116:9999/apart', {
+            params: {
+                apart_name: keyword
+            },
+        }).then((res)=>{
+            console.log(keyword);
+            console.log(res);
+        }).catch((res)=>{
+            console.log(keyword);
+            console.log(res);
+        });
 
-    if (loading) {
-        return <div>로딩중...</div>;
-    }
+    }, [keyword])
+    return null;
+    // if (keyword.length < 2) {
+    //     return null;
+    // }
 
-    if (error) {
-        return <div>에러!!!</div>;
-    }
+    // if (loading) {
+    //     return <div>로딩중...</div>;
+    // }
 
-    if (!aparts) {
-        return null;
-    }
+    // if (error) {
+    //     return <div>에러!!!</div>;
+    // }
 
-    return (
-        <Wrapper>
-            <div className="HeadAutoCompleteItem">
-                <ul className="Items">
-                    {aparts.map((apart) =>
-                        <li onClick={() => suggestionSelected(apart)}>
-                            <Link to="/apartInfo">
-                                {apart.name}
-                                <small>
-                                    <span>
-                                        {apart.address_1}
-                                    </span>
-                                </small>
-                            </Link>
-                        </li>
-                    )}
-                </ul>
-            </div>
-        </Wrapper>
-    )
+    // if (!aparts) {
+    //     return null;
+    // }
+
+    // return (
+    //     <Wrapper>
+    //         <div className="HeadAutoCompleteItem">
+    //             <ul className="Items">
+    //                 {aparts.map((apart, index) =>
+    //                     <li key={index} onClick={() => suggestionSelected(apart)}>
+    //                         <Link to="/apartInfo">
+    //                             {apart.name}
+    //                             <small>
+    //                                 <span>
+    //                                     {apart.address_1}
+    //                                 </span>
+    //                             </small>
+    //                         </Link>
+    //                     </li>
+    //                 )}
+    //             </ul>
+    //         </div>
+    //     </Wrapper>
+    // )
 }
 
 export default Aparts;
