cb6704665ac0be61ee042baf7a4556fabedc01eb
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-08-21 13:30:09 +0900

    use conditional useEffect

--- client/src/data/Aparts.js
+++ client/src/data/Aparts.js
@@ -1,18 +1,19 @@
-import React, { } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useAsync } from '../util/useAsync';
 import { Link } from 'react-router-dom';
 import { Wrapper } from './Aparts.css'
-import {env} from '../common/Env.js';
+import { env } from '../common/Env.js';
+
+let finalKeyword;
+let isStopRender = false;
 
 async function getAsyncAparts(keyword) {
-    console.log(keyword);
     const response = await axios.get(env.serverAddress + '/apart', {
         params: {
             apart_name: keyword
         },
     });
-    console.log("async : " + keyword);
     return response.data;
 }
 
@@ -22,45 +23,79 @@ function suggestionSelected(value) {
 
 function Aparts(props) {
     const keyword = props.keyword;
-    const [state, refetch] = useAsync(() => getAsyncAparts(props.keyword), [props.keyword]);
-    const { loading, data: aparts, error } = state;
+    finalKeyword = keyword;
+    const [render, setRender] = useState(null);
 
-    if (keyword.length < 2) {
-        return null;
-    }
+    useEffect(()=>{
+        if(keyword.length > 1){
+            getAsyncAparts(keyword).then((res)=>{
+                if(keyword === finalKeyword){
+                    isStopRender = true;
+                }
+                if(isStopRender){
+                    setRender(
+                        <Wrapper>
+                        <div className="HeadAutoCompleteItem">
+                            <ul className="Items">
+                                {res.map((apart, index) =>
+                                    <li key={index} onClick={() => suggestionSelected(apart)}>
+                                        <Link to="/apartInfo">
+                                            {apart.name}
+                                            <small>
+                                                <span>
+                                                    {apart.address_1} {apart.address_2} {apart.address_3}
+                                                </span>
+                                            </small>
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    </Wrapper>
+                    );
+                    isStopRender = false;
+                }
+            })
+        }
+    }, [keyword]);
 
-    if (loading) {
-        return <div>로딩중...</div>;
-    }
+    return render;
+    // if (keyword.length < 2) {
+    //     return null;
+    // }
 
-    if (error) {
-        return <div>에러!!!</div>;
-    }
+    // if (loading) {
+    //     return <div>로딩중...</div>;
+    // }
 
-    if (!aparts) {
-        return null;
-    }
+    // if (error) {
+    //     return <div>에러!!!</div>;
+    // }
 
-    return (
-        <Wrapper>
-            <div className="HeadAutoCompleteItem">
-                <ul className="Items">
-                    {aparts.map((apart, index) =>
-                        <li key={index} onClick={() => suggestionSelected(apart)}>
-                            <Link to="/apartInfo">
-                                {apart.name}
-                                <small>
-                                    <span>
-                                        {apart.address_1} {apart.address_2} {apart.address_3}
-                                    </span>
-                                </small>
-                            </Link>
-                        </li>
-                    )}
-                </ul>
-            </div>
-        </Wrapper>
-    )
+    // if (!aparts) {
+    //     return null;
+    // }
+
+    // return (
+        // <Wrapper>
+        //     <div className="HeadAutoCompleteItem">
+        //         <ul className="Items">
+        //             {aparts.map((apart, index) =>
+        //                 <li key={index} onClick={() => suggestionSelected(apart)}>
+        //                     <Link to="/apartInfo">
+        //                         {apart.name}
+        //                         <small>
+        //                             <span>
+        //                                 {apart.address_1} {apart.address_2} {apart.address_3}
+        //                             </span>
+        //                         </small>
+        //                     </Link>
+        //                 </li>
+        //             )}
+        //         </ul>
+        //     </div>
+        // </Wrapper>
+    // )
 }
 
 export default Aparts;
